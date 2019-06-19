2c616b40c4b6a4003458857901fdeed55090faf3
van jordan <vanjordan46@hotmail.com>
2019-06-19 15:47:07 -0700

    all endpoints done but cant connect to server

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
+const userRouter = require('./users/userRouter.js');
 const server = express();
 
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.use('api/users', userRouter)
+server.use('api/posts', postRouter)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
-
+ 
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`Method: ${req.method} URL: ${req.url} date: ${Date.now()}`)
 };
 
 module.exports = server;
